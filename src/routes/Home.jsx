dad23752c402775fc4435f4edf129c274b7754d5
Aymane El M'hamdi <aymanelmhamdi123@gmail.com>
2026-02-28 01:33:09 +0000

    ---1

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -5,6 +5,7 @@ import { LiaCitySolid } from "react-icons/lia";
 import { CiTempHigh } from "react-icons/ci";
 import { TbMathMax } from "react-icons/tb";
 import { TbMathMin } from "react-icons/tb";
+import { FaAudioDescription } from "react-icons/fa";
 
 
 export default function Today() {
@@ -13,11 +14,11 @@ export default function Today() {
 
 
     return (
-        <div className="flex flex-row justify-between  gap-5 h-screen w-auto">
+        <div className="flex flex-row justify-between  bg-gray-100 gap-5 h-screen w-auto">
             <div className="flex flex-col pl-5">
                 <h1 className="text-3xl font-bold p-5 flex flex -row"><LiaCitySolid className="m-1 mr-1" /> {weather?.name}</h1>
 
-                <div className="flex flex-row gap-9 p-2 ">
+                <div className="flex flex-row gap-9 p-2 border-t-1 ">
                     <div className="border-r-1 p-5  flex flex-col font-bold text-xl ">
                         <p className="flex flex-row"> Temp :<CiTempHigh className="m-1" /></p>
                         <p>{weather?.main?.temp} °C</p>
@@ -33,8 +34,13 @@ export default function Today() {
                         <p>{weather?.main?.temp_min} °C</p>
 
                     </div>
+                 
 
 
+                </div>
+                <div  className="text-xl font-bold p-5  flex gap-1 border-t-1">
+                    <p className="flex "> <FaAudioDescription className="m-1"/> Description :</p>
+                        <p className="font-thick ">{weather?.weather[0].description}</p>
                 </div>
 
             </div>
--- src/routes/Layout.jsx
+++ src/routes/Layout.jsx
@@ -5,22 +5,23 @@ import NavBar from "../components/NavBar";
 import { useContext } from "react";
 import { WeatherContext } from "../context/WeatherContext";
 export default function Layout() {
-  const {setCity}=useContext(WeatherContext)
+  const { setCity } = useContext(WeatherContext)
   return (
     <div className="flex ">
-      <Menu />
-      <div className="flex justify-center w-full h-screen bg-gray-100 m-2">
-        <div className=" flex flex-col w-full h-full">
-           <NavBar setCity={setCity}/>
-           <Outlet  />
 
-        </div>
-          
+            <Menu />
+            <div className="flex justify-center w-full h-screen bg-gray-100 m-2">
+              <div className=" flex flex-col w-full h-full">
+                <NavBar setCity={setCity} />
+                <Outlet />
 
-          
-      </div>
-      
-      
-    </div>
-  );
+              </div>
+
+
+
+            </div>
+
+
+          </div>
+          );
 }
